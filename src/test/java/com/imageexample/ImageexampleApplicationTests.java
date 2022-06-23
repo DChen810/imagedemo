d0ffbf7c31f81d371bf5f7fcb567629c62cdbf1d
DChen810 <cdk970218@163.com>
2022-06-23 13:09:49 -0500

    Project Upload

---
+++ src/test/java/com/imageexample/ImageexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imageexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
